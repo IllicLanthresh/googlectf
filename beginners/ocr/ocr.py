755274ab8c9e18dfdb12147496d2594c8c611a4a
IllicLanthresh <miqueldreamer@gmail.com>
2018-06-24 04:37:23 +0200

    formatting

--- beginners/ocr/ocr.py
+++ beginners/ocr/ocr.py
@@ -1,36 +1,36 @@
 import re
 
 dictionary = {
-    'r':'y',
-    'k':'r',
-    'd':'k',
-    'w':'d',
-    'p':'w',
-    'i':'p',
-    'b':'i',
-    'u':'b',
-    'n':'u',
-    'g':'n',
-    'z':'g',
-    's':'z',
-    'l':'s',
-    'e':'l',
-    'x':'e',
-    'v':'c',
-    'o':'v',
-    'h':'o',
-    'a':'h',
-    't':'a',
-    'm':'t',
-    'f':'m',
-    'Y':'f',
-    'q':'x'
+    'r': 'y',
+    'k': 'r',
+    'd': 'k',
+    'w': 'd',
+    'p': 'w',
+    'i': 'p',
+    'b': 'i',
+    'u': 'b',
+    'n': 'u',
+    'g': 'n',
+    'z': 'g',
+    's': 'z',
+    'l': 's',
+    'e': 'l',
+    'x': 'e',
+    'v': 'c',
+    'o': 'v',
+    'h': 'o',
+    'a': 'h',
+    't': 'a',
+    'm': 't',
+    'f': 'm',
+    'Y': 'f',
+    'q': 'x'
 }
-c=[]
+c = []
 
-for a,b in dictionary.items():
+for a, b in dictionary.items():
     c.append(b)
-    print(a,",",b,": ", end="")
+    print(a, ",", b, ": ", end="")
     print(a in c)
 
 with open("ocred") as file:
@@ -40,9 +40,9 @@ decrypted = original_text
 
 for cry, decry in dictionary.items():
     regx = re.compile(re.escape(cry), re.IGNORECASE)
-    
+
     #decry = "\033[31m" + decry + "\033[m"
-    
-    decrypted = regx.sub(decry , decrypted)
+
+    decrypted = regx.sub(decry, decrypted)
 
 print(decrypted)
